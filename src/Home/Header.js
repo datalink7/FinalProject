ff7b16345588228528efd25d5fb2306828c84c85
datalink7 <63325701+datalink7@users.noreply.github.com>
2020-06-08 16:28:52 +0900

    menu upload
    menu upload

--- src/Home/Header.js
+++ src/Home/Header.js
@@ -1,25 +1,25 @@
 import React from 'react';
 
 const Header = () => (
-    <div>
-        <ul>
-            <li>
-                <a href="#none">Match</a>
-            </li>
-            <li>
-                <a href="#none">League</a>
-            </li>
-            <li>
-                <a href="#none">Team</a>
-            </li>
-            <li>
-                <a href="#none">Proxy</a>
-            </li>
-            <li>
-                <a href="#none">Q & A</a>
-            </li>
-        </ul>
-    </div>
+        <div className="Menu">
+            <ul>
+                <li>
+                    <a href="#none">Match</a>
+                </li>
+                <li>
+                    <a href="#none">League</a>
+                </li>
+                <li>
+                    <a href="#none">Team</a>
+                </li>
+                <li>
+                    <a href="#none">Proxy</a>
+                </li>
+                <li>
+                    <a href="#none">Q & A</a>
+                </li>
+            </ul>
+        </div>
 )
 
 export default Header;
--- src/Home/Main.js
+++ src/Home/Main.js
@@ -3,13 +3,15 @@ import Footer from './Footer';
 import Header from './Header';
 import ReactPlayer from 'react-player/youtube';
 
+import '../Css/Style.css'
+
 class Main extends Component{
 
     render(){
         return(
             <div>
                 <Header/>
-                <ReactPlayer url='https://youtu.be/ep16_YDfNUU'/>
+                    <ReactPlayer url='https://youtu.be/ep16_YDfNUU' playing control/>
                 <Footer/>
             </div>
         )
