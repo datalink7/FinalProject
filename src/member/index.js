21fb5d03d4a4ce234eb744248c7764d75fbd9c2a
jyyoun111 <66234667+jyyoun111@users.noreply.github.com>
2020-06-08 16:45:48 +0900

    2020060804-45

---
+++ src/member/index.js
@@ -0,0 +1,15 @@
+import React,{Component} from 'react';
+import router from 'react-router-dom';
+
+class Ex1App extends Component{
+
+    render(){
+        return(
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default Ex1App
