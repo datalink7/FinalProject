3cc4f426f376bfa497d899f2482a0b37483d0747
jyyoun111 <66234667+jyyoun111@users.noreply.github.com>
2020-06-15 17:28:35 +0900

    afa
    aaf

--- src/index.js
+++ src/index.js
@@ -1,27 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-// import List from './Board/List.js';
-import Write from './Board/Write.js';
-//  import LoginForm from './member/LoginForm';
-//  import JoinForm from './member/JoinForm';
-//  import JoinFinal from './member/JoinFinal';
-  // import Find from './member/Find';
-  // import Root from './member/Root';
-
-
+// import './index.css';
+// import LoginForm from './member/LoginForm';
+// import JoinForm from './member/JoinForm';
+// import Root from './Root';
+import Write from './Board/Write';
 import * as serviceWorker from './serviceWorker';
-
 
 ReactDOM.render(
   <React.StrictMode>
     <Write/>
-    {/* <List/> */}
     {/* <Root/> */}
-      {/* <LoginForm/>    */}
-       {/* <JoinForm/>  
-      <JoinFinal/>   */}
-      {/* <Find/> */}
+    {/* <LoginForm /> */}
+    {/* <JoinForm/> */}
   </React.StrictMode>,
   document.getElementById('root')
 );
